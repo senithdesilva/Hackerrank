130a50d3c988711bf4b201402a016d6ee9096fa4
Senith De Silva <senith.desilva@yahoo.co.uk>
2019-01-28 11:55:40 +1100

    Java Inheritance I solution

---
+++ ObjectOrientedProgramming/src/com/senith/hackerrank/oop/ch01/Animal.java
@@ -0,0 +1,7 @@
+package com.senith.hackerrank.oop.ch01;
+
+class Animal{
+    void walk(){
+        System.out.println("I am walking");
+    }
+}
---
+++ ObjectOrientedProgramming/src/com/senith/hackerrank/oop/ch01/Bird.java
@@ -0,0 +1,10 @@
+package com.senith.hackerrank.oop.ch01;
+
+class Bird extends Animal {
+    void fly(){
+        System.out.println("I am flying");
+    }
+    void sing() {
+        System.out.println("I am singing");
+    }
+}
