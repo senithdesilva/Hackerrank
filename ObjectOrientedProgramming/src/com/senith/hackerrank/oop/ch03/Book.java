81987b4d789d1f1d53128204dc3e6985b8fe7a15
Senith De Silva <senith.desilva@yahoo.co.uk>
2019-01-28 20:45:03 +1100

    Java Abstract Class Solution!

---
+++ ObjectOrientedProgramming/src/com/senith/hackerrank/oop/ch03/Book.java
@@ -0,0 +1,11 @@
+package com.senith.hackerrank.oop.ch03;
+
+abstract class Book {
+    String title;
+
+    abstract void setTitle(String s);
+
+    String getTitle() {
+        return title;
+    }
+}
---
+++ ObjectOrientedProgramming/src/com/senith/hackerrank/oop/ch03/MyBook.java
@@ -0,0 +1,8 @@
+package com.senith.hackerrank.oop.ch03;
+
+class MyBook extends Book{
+    @Override
+    void setTitle(String s) {
+        title = s;
+    }
+}
