411a70bd7db60e31096b48f16fe38a66af5db33c
Senith De Silva <senith.desilva@yahoo.co.uk>
2019-01-28 20:23:58 +1100

    Java Inheritance II solution!

---
+++ ObjectOrientedProgramming/src/com/senith/hackerrank/oop/ch02/Adder.java
@@ -0,0 +1,4 @@
+package com.senith.hackerrank.oop.ch02;
+
+class Adder extends Arithmetic{
+}
---
+++ ObjectOrientedProgramming/src/com/senith/hackerrank/oop/ch02/Arithmetic.java
@@ -0,0 +1,7 @@
+package com.senith.hackerrank.oop.ch02;
+
+class Arithmetic {
+    Integer add(int a, int b) {
+        return a+b;
+    }
+}
