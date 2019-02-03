a3e43463dcae968f306a537aeff55f7cef9f17ef
Senith De Silva <senith.desilva@yahoo.co.uk>
2019-02-03 18:27:08 +1100

    Java Exception Handling Solution

---
+++ ExceptionHandling/src/com/senith/hackerrank/MyCalculator.java
@@ -0,0 +1,14 @@
+package com.senith.hackerrank;
+
+class MyCalculator {
+    static String power(int n, int p) throws Exception {
+        if (n < 0 || p < 0) {
+            throw new Exception("n or p should not be negative.");
+        }
+        if (n == 0 && p == 0) {
+            throw new Exception("n and p should not be zero.");
+        }
+
+        return String.valueOf((int)Math.pow(n, p));
+    }
+}
