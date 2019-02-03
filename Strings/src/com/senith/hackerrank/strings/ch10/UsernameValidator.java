6745690a73b324a7567efa94a5f40f2326ca6c79
Senith De Silva <senith.desilva@yahoo.co.uk>
2019-02-03 11:44:57 +1100

    Valid Username Regular Expression Solution

---
+++ Strings/src/com/senith/hackerrank/strings/ch10/UsernameValidator.java
@@ -0,0 +1,8 @@
+package com.senith.hackerrank.strings.ch10;
+
+public class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = "^[a-zA-Z][a-zA-Z0-9_]{7,29}$";
+}
